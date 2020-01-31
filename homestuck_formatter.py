9ee8c998fa15d375aa32df8e0807c7cc9cc17cd8
Paravellex <60281890+Paravellex@users.noreply.github.com>
2020-01-31 01:26:05 -0800

    Update homestuck_formatter.py

--- homestuck_formatter.py
+++ homestuck_formatter.py
@@ -1,75 +1,84 @@
 # Homestuck Character Formatter by Paravellex
-# Last modified 1/24/2020
+# Last modified 1/31/2020
 # Feel free to use for your own projects
+
+# INSTRUCTIONS FOR USE:
+# Install Python.
+# Save this file in the same directory as a text file containing your fic.
+# Open a command terminal in that directory.
+# Type the following command:
+#	python homestuck_formatter.py FIC_FILE.txt
+# where "FIC_FILE.txt" is replaced by whatever name your fic file is.
+# The formatted fic will be located in "formatted_FIC_FILE.txt or similar.
 
 import sys
 
 char_dict = {
     # Humans
-    'JOHN:': 'john',
-    'JUNE:': 'john',
-    'ROSE:': 'rose',
-    'DAVE:': 'dave',
-    'JADE:': 'jade',
-    'JANE:': 'jane',
-    'ROXY:': 'roxy',
-    'DIRK:': 'dirk',
-    'JAKE:': 'jake',
+    'JOHN': 'john',
+    'JUNE': 'john',
+    'ROSE': 'rose',
+    'DAVE': 'dave',
+    'JADE': 'jade',
+    'JANE': 'jane',
+    'ROXY': 'roxy',
+    'DIRK': 'dirk',
+    'JAKE': 'jake',
     # Beta trolls
-    'ARADIA:': 'aradia',
-    'TAVROS:': 'tavros',
-    'SOLLUX:': 'sollux',
-    'KARKAT:': 'karkat',
-    'NEPETA:': 'nepeta',
-    'KANAYA:': 'kanaya',
-    'TEREZI:': 'terezi',
-    'VRISKA:': 'vriska',
-    'EQUIUS:': 'equius',
-    'GAMZEE:': 'gamzee',
-    'ERIDAN:': 'eridan',
-    'FEFERI:': 'feferi',
+    'ARADIA': 'aradia',
+    'TAVROS': 'tavros',
+    'SOLLUX': 'sollux',
+    'KARKAT': 'karkat',
+    'NEPETA': 'nepeta',
+    'KANAYA': 'kanaya',
+    'TEREZI': 'terezi',
+    'VRISKA': 'vriska',
+    'EQUIUS': 'equius',
+    'GAMZEE': 'gamzee',
+    'ERIDAN': 'eridan',
+    'FEFERI': 'feferi',
     # Alpha trolls
-    'DAMARA:': 'aradia',
-    'RUFIOH:': 'tavros',
-    'MITUNA:': 'sollux',
-    'KANKRI:': 'kankri',  # The only different one
-    'MEULIN:': 'nepeta',
-    'PORRIM:': 'kanaya',
-    'LATULA:': 'terezi',
-    'ARANEA:': 'vriska',
-    'HORUSS:': 'equius',
-    'KURLOZ:': 'gamzee',
-    'CRONUS:': 'eridan',
-    'MEENAH:': 'feferi',
+    'DAMARA': 'aradia',
+    'RUFIOH': 'tavros',
+    'MITUNA': 'sollux',
+    'KANKRI': 'kankri',  # The only different one
+    'MEULIN': 'nepeta',
+    'PORRIM': 'kanaya',
+    'LATULA': 'terezi',
+    'ARANEA': 'vriska',
+    'HORUSS': 'equius',
+    'KURLOZ': 'gamzee',
+    'CRONUS': 'eridan',
+    'MEENAH': 'feferi',
     # Cherubs
-    'CALLIOPE:': 'calliope',  # If alt!Calliope, change to kankri
-    'CALIBORN:': 'caliborn',
+    'CALLIOPE': 'calliope',  # If alt!Calliope, change to kankri
+    'CALIBORN': 'caliborn',
     # Sprites (not incl. sprite^2's)
-    'NANNASPRITE:': 'jane',
-    'JASPERSPRITE:': 'jaspersprite',  # Not the same as Roxy's color!
-    'DAVESPRITE:': 'dirk',
-    'JADESPRITE:': 'jake',
-    'GCATAVROSPRITE:': 'john',
-    'FEFETASPRITE:': 'rose',
-    'ARQUIUSPRITE:': 'dave',
-    'ERISOLSPRITE:': 'jade',
+    'NANNASPRITE': 'jane',
+    'JASPERSPRITE': 'jaspersprite',  # Not the same as Roxy's color!
+    'DAVESPRITE': 'dirk',
+    'JADESPRITE': 'jake',
+    'GCATAVROSPRITE': 'john',
+    'FEFETASPRITE': 'rose',
+    'ARQUIUSPRITE': 'dave',
+    'ERISOLSPRITE': 'jade',
     # Epilogue and HS2 characters
-    '(VRISKA):': 'vriska',
-    'SWIFER:': 'kanaya',
-    'CLIPER:': 'kanaya',
-    'VRISSY:': 'vriska',
-    'HARRY ANDERSON:': 'harry',
-    'HARRY:': 'harry',
+    '(VRISKA)': 'vriska',
+    'SWIFER': 'kanaya',
+    'CLIPER': 'kanaya',
+    'VRISSY': 'vriska',
+    'HARRY ANDERSON': 'harry',
+    'HARRY': 'harry',
     # doc scratch isn't supported by this program
 }
 
 spritesquared_dict = {
     # Spritesquareds
-    # Number = characters after the colon to color the same as the name
+    # Number = characters after the name to color the same
     # First string: color of the name
     # Second string: color of the speech
-    'JASPROSESPRITE^2:': [0, 'jaspersprite', 'rose'],
-    'DAVEPETASPRITE^2:': [6, 'jade', 'dirk']  # Meat 15 uses nepeta, not jade
+    'JASPROSESPRITE^2': [1, 'jaspersprite', 'rose'],
+    'DAVEPETASPRITE^2': [7, 'jade', 'dirk']  # Meat 15 uses nepeta, not jade
 }
 
 input = open(sys.argv[1], 'r', encoding="utf8")
@@ -83,32 +92,32 @@ for line_raw in input:
         in_block = False
         continue
     line = line_raw.rstrip()
-    word = line.split(' ', 1)[0]
-    if word in char_dict.keys():
+    prec = line.split(':', 1)[0]  # Pre-Colon
+    if prec in char_dict.keys():
         if not in_block:
             output.write("<p class=\"block\">")
             in_block = True
         else:
             output.write("<br />\n")
         output.write("<span class=\"")
-        output.write(char_dict[word])
+        output.write(char_dict[prec])
         output.write("\">")
         output.write(line)
         output.write("</span>")
-    elif word in spritesquared_dict.keys():
+    elif prec in spritesquared_dict.keys():
         if not in_block:
             output.write("<p class=\"block\">")
             in_block = True
         else:
             output.write("<br />\n")
         output.write("<span class=\"")
-        output.write(spritesquared_dict[word][1])
+        output.write(spritesquared_dict[prec][1])
         output.write("\">")
-        output.write(line[:len(word) + spritesquared_dict[word][0]])
+        output.write(line[:len(prec) + spritesquared_dict[prec][0]])
         output.write("</span><span class=\"")
-        output.write(spritesquared_dict[word][2])
+        output.write(spritesquared_dict[prec][2])
         output.write("\">")
-        output.write(line[len(word) + spritesquared_dict[word][0]:])
+        output.write(line[len(prec) + spritesquared_dict[prec][0]:])
         output.write("</span>")
     else:
         if in_block:
